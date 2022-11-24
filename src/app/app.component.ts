83028317af1c59eb7e951aa8ec18081db3e10659
Bianca Antoci <73387217+bianca-antoci@users.noreply.github.com>
2022-11-24 10:42:49 +0000

    feat: implement the first api call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RequestsService } from './requests.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'DailyNews';
+export class AppComponent implements OnInit {
+
+  constructor(
+    private service: RequestsService,
+  ) { }
+
+  ngOnInit(): void {
+    this.getListOfFilms();
+  }
+  /**
+   * this function fetch films
+   */
+  getListOfFilms() {
+    this.service.getListFilms().subscribe(
+      (data: any) => {
+        console.log(data);
+      },
+
+    );
+  }
 }
--- src/app/requests.service.ts
+++ src/app/requests.service.ts
@@ -6,6 +6,18 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 	providedIn: "root",
 })
 export class RequestsService {
+	base = `https://swapi.dev/api`;
+	constructor(private service: HttpClient) { }
 
+	headers() {
+		const header: any = {};
+		return new HttpHeaders(header);
+	}
 
+	getListFilms(): any {
+		const requestHeaders = this.headers();
+		return this.service.get<any>(`${this.base}/films`, {
+			headers: requestHeaders,
+		})
+	}
 }
